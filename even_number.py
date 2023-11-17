ffb9e494ff4e3ed8d324f414b843a38070fac595
Dana S <143336801+dana-seb@users.noreply.github.com>
2023-11-17 14:38:10 -0500

    Delete even_number.py

--- even_number.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter a number! "))
-
-# If number is divisible by two
-if number % 2 == 0:
-    print("The number entered is an even number!")
-else:
-    print("The number entered is odd!")
